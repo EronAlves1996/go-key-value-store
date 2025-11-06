97e68235ef6e313ad6b5c0fbcd9bf9f0c7e6c98e
Eron Alves <eron.alves@rocketmail.com>
2025-11-06 17:12:51 -0300

    feat: implement the set method

--- main.go
+++ main.go
@@ -7,3 +7,10 @@ type KVStore struct {
 	data map[string]string
 	file string
 }
+
+func (k *KVStore) Set(key, value string) error {
+	k.mu.Lock()
+	defer k.mu.Unlock()
+	k.data[key] = value
+	return nil
+}
