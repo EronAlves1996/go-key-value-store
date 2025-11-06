7bac5676d9ef80ad82ae1a165bfe366493d00bb4
Eron Alves <eron.alves@rocketmail.com>
2025-11-06 17:15:32 -0300

    feat: create the get method

--- main.go
+++ main.go
@@ -1,16 +1,36 @@
 package main
 
-import "sync"
+import (
+	"errors"
+	"sync"
+)
 
 type KVStore struct {
-	mu   sync.Mutex
+	mu   sync.RWMutex
 	data map[string]string
 	file string
+}
+
+func (k *KVStore) save() error {
+	return nil
 }
 
 func (k *KVStore) Set(key, value string) error {
 	k.mu.Lock()
 	defer k.mu.Unlock()
+	defer k.save()
 	k.data[key] = value
 	return nil
+}
+
+func (k *KVStore) Get(key string) (string, error) {
+	k.mu.RLock()
+	defer k.mu.RUnlock()
+	v, ok := k.data[key]
+
+	if !ok {
+		return "", errors.New("key not found")
+	}
+
+	return v, nil
 }
