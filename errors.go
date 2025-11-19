78fb15b71dab323e746d77235c6839715af086d9
Eron Alves <eron.alves@rocketmail.com>
2025-11-18 22:55:49 -0300

    feat: implements Is and As

--- errors.go
+++ errors.go
@@ -9,3 +9,17 @@ type InvalidKeyError struct {
 func (i *InvalidKeyError) Error() string {
 	return fmt.Sprintf("invalid key: %v", i.Key)
 }
+
+func (i *InvalidKeyError) Is(target error) bool {
+	converted, ok := target.(*InvalidKeyError)
+	if !ok {
+		return false
+	}
+
+	return converted.Key == i.Key
+}
+
+func (i *InvalidKeyError) As(t any) bool {
+	_, ok := t.(*InvalidKeyError)
+	return ok
+}
